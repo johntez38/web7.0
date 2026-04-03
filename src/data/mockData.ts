bb6a9155f7aa3e85d65d2c224cab68ef225f9ae6
John Mureithi <mureithijohn117@gmail.com>
2026-04-03 17:38:09 +0300

    Create mockData.ts

---
+++ src/data/mockData.ts
@@ -0,0 +1,18 @@
+export const properties = [
+  {
+    id: 1,
+    name: "2 Bedroom Apartment",
+    location: "Nairobi",
+    price: 25000,
+    lat: -1.2921,
+    lng: 36.8219
+  },
+  {
+    id: 2,
+    name: "Bedsitter",
+    location: "Westlands",
+    price: 15000,
+    lat: -1.2676,
+    lng: 36.8108
+  }
+];
