d8772e06630e1e81fa071b3cdcc07c5fd46f41d9
John Mureithi <mureithijohn117@gmail.com>
2026-04-03 17:32:45 +0300

    Create routes.tsx

---
+++ src/routes.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from "react-router";
+import { HomePage } from "./pages/HomePage";
+
+export const router = createBrowserRouter([
+  { path: "/", Component: HomePage }
+]);
